fdb47d86881a8cff6a420e344a97a217428eb0f1
Steve Fallet <steve.fallet@divtec.ch>
2021-04-28 08:14:06 +0200

    ajout composant EnteteDePage.vue et PiedDePage.vue

---
+++ src/components/EnteteDePage.vue
@@ -0,0 +1,39 @@
+<template>
+  <header class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
+    <!-- Logo -->
+    <router-link to="/"
+       class="d-flex align-items-center text-dark text-decoration-none">
+      <img src="../assets/img/sabre-laser.svg" alt="Logo Sabres Laser Sàrl"
+           class="logo">
+      <span class="fs-4">Sabres Lasers Sàrl</span>
+    </router-link>
+
+    <!-- Menu de navigation -->
+    <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+      <router-link class="me-3 py-2 text-dark text-decoration-none"
+         to="/"
+      >Accueil</router-link>
+
+      <router-link class="me-3 py-2 text-dark text-decoration-none"
+         to="/produits"
+      >Nos Produits</router-link>
+
+      <router-link class="me-3 py-2 text-dark text-decoration-none"
+         to="panier"
+      >Ton Panier ({{ totalPanier }} CHF)</router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'EnteteDePage',
+  props: {
+    totalPanier: Number,
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/PiedDePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="pt-4 my-md-5 pt-md-5 border-top">
+    <small class="d-block mb-3 text-muted">
+      &copy;2021 - Sabres Laser Sàrl
+    </small>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'PiedDePage'
+}
+</script>
